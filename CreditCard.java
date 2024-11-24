f2ce634c49cb81465b21764dca304ec9a4ccdb83
theta148 <mihusunny@gmail.com>
2024-11-24 16:50:45 -0500

    Add files via upload

--- CreditCard.java
+++ CreditCard.java
@@ -2,6 +2,35 @@ public class CreditCard
 {
     private Person owner;
     private Money balance;
+    private Money creditLimit;
 
+    public CreditCard(Person owner, Money creditLimit)
+    {
+        this.owner = owner;
+        this.creditLimit = creditLimit;
+        this.balance = new Money(0.00);
+    }
 
+    public getBalance()
+    {
+        return balance;
+    }
+
+    public getCreditLimit()
+    {
+        return creditLimit;
+    }
+
+    public getPersonals()
+    {
+        return owner.toString();
+    }
+
+    public charge(Money amount)
+    {
+        if (balance == creditLimit)
+        {
+
+        }
+    }
 }
