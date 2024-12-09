1435851e753f417f24bbeb9cc939f677105fd2cf
theta148 <mihusunny@gmail.com>
2024-12-08 22:52:15 -0500

    Add files via upload

--- CompactDisc.java
+++ CompactDisc.java
@@ -8,27 +8,28 @@ public class CompactDisc
 {
     public static void main(String[] args)throws IOException
     {
-        Scanner input = new Scanner(new File("Classics.txt"));
+        Scanner input = new Scanner(new File("./src/Classics.txt"));
         String title;
         String artist;
-        // ADD LINES FOR TASK #3 HERE
+
+
         Song[] cd = new Song[6];
         for (int i = 0; i < cd.length; i++)
         {
             title = input.nextLine();
             artist = input.nextLine();
-            // ADD LINES FOR TASK #3 HERE
             // Fill the array by creating a new song with
             // the title and artist and storing it in the
             // appropriate position in the array
-            cd[i] = ("%s %s", title, artist);
+            Song song = new Song(title, artist);
+            cd[i] = song;
         }
         System.out.println("Contents of Classics:");
         for (int i = 0; i < cd.length; i++)
         {
-            // ADD LINES FOR TASK #3 HERE
+           
             // Print the contents of the array to the console
-            System.out.print("%s by %s\n", title, artist);
+            System.out.print(cd[i]);
         }
     }
 }
